9bcc15adc0718422d84f222d26ff1151e1c38858
sonalisaha781185-byte <sonalisaha781185@gmail.com>
2025-09-27 17:12:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let result= document.querySelector(.'result');
+function appendToResult(value){
+    result.value+=value;
+}
+function clearResult|(){
+    result.value='';
+}
+function calculateResult(){
+    result.value=eval(result.value);
+}
